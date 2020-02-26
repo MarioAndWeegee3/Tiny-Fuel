bd4068569850acbcb264a5d3b894217bcafe8a7e
MarioAndWeegee3 <35458805+MarioAndWeegee3@users.noreply.github.com>
2020-02-26 01:02:13 -0500

    Cleaning up
    Also added a license and an easter egg

--- src/main/java/marioandweegee3/tinyfuel/TinyFuel.java
+++ src/main/java/marioandweegee3/tinyfuel/TinyFuel.java
@@ -1,6 +1,5 @@
 package marioandweegee3.tinyfuel;
 
-import marioandweegee3.tinyfuel.common.items.Fuels;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.item.Item;
@@ -11,21 +10,22 @@ import net.minecraft.util.registry.Registry;
 public class TinyFuel implements ModInitializer{
     public static final String modID = "tinyfuel";
 
-    @Override
-    public void onInitialize() {
-        registerItem(Fuels.tiny_coal, "tiny_coal", ItemGroup.MISC, 200);
-        registerItem(Fuels.tiny_charcoal, "tiny_charcoal", ItemGroup.MISC, 200);
-    }
-    
-    public static void registerItem(Item item, String name, ItemGroup group){
-        registerItem(item, name, group, 0);
+    public static void main(String[] args) {
+        System.out.println("What are you doing trying to execute a mod?");
+        System.out.println("Don't you know these don't work without Minecraft?");
     }
 
-    public static void registerItem(Item item, String name, ItemGroup group, int cooktime){
-        item = new Item(makeSettings().group(group));
+    @Override
+    public void onInitialize() {
+        registerItem("tiny_coal", ItemGroup.MISC, 200);
+        registerItem("tiny_charcoal", ItemGroup.MISC, 200);
+    }
+
+    public static void registerItem(String name, ItemGroup group, int cookTime){
+        Item item = new Item(makeSettings().group(group));
         Registry.register(Registry.ITEM, makeID(name), item);
-        if(cooktime > 0)
-            FuelRegistry.INSTANCE.add(item, cooktime);
+        if(cookTime > 0)
+            FuelRegistry.INSTANCE.add(item, cookTime);
     }
 
     public static Item.Settings makeSettings(){
--- src/main/java/marioandweegee3/tinyfuel/common/items/Fuels.java
+++
@@ -1,8 +0,0 @@
-package marioandweegee3.tinyfuel.common.items;
-
-import net.minecraft.item.Item;
-
-public class Fuels{
-    public static Item tiny_coal;
-    public static Item tiny_charcoal;
-}
